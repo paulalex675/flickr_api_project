851248fbbf14bd89bb7336b8224f670da0df9e37
Paul Robinson <paulalex675@hotmail.com>
2021-04-01 19:27:35 +0100

    can't get figaro to work, api keys work but not when hidden

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,17 @@ class StaticPagesController < ApplicationController
   require 'flickr'
 
   def index
-    flickr = Flickr.new "flickr_api_key:", "flickr_secret:"
+    begin
+      flickr = Flickr.new "FLICKR_API_KEY", "FLICKR_SHARED_SECRET"
+
+      unless params[:user_id].blank?
+        @photos = flickr.photos.search(user_id: params[:user_id])
+      else
+        @photos = flickr.photos.getRecent
+      end
+    rescue StandardError => e
+      flash[:alert] = "#{e.class}: #{e.message}. Please try again..."
+      redirect_to root_path
+    end
   end
 end
